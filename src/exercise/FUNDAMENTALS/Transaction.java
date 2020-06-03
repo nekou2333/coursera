7f80774f21459c70e8ce511b1efcc0c1b198c6e6
zhaoyuxuan <929644473@qq.com>
2020-06-04 00:08:40 +0800

    Optimizing class Transaction

--- src/exercise/FUNDAMENTALS/Transaction.java
+++ src/exercise/FUNDAMENTALS/Transaction.java
@@ -1,6 +1,6 @@
 package exercise.FUNDAMENTALS;
 
-public class Transaction {
+public class Transaction implements Comparable<Transaction> {
     private final String who;
     private final Date when;
     private final double amount;
@@ -11,6 +11,16 @@ public class Transaction {
         this.who = who;
         this.when = when;
         this.amount = amount;
+    }
+
+    public Transaction(String transaction) {
+        String[] fields = transaction.split("\\s+");
+        if (fields.length != 3) throw new IllegalArgumentException("Invalid Transaction");
+        who = fields[0];
+        when = new Date(fields[1]);
+        amount = Double.parseDouble(fields[2]);
+        if (Double.isNaN(amount) || Double.isInfinite(amount))
+            throw new IllegalArgumentException("Amount cannot be NaN or infinite");
     }
 
     public String who() {
@@ -29,13 +39,17 @@ public class Transaction {
         return String.format("%-10s %10s %8.2f", who, when, amount);
     }
 
+    public int compareTo(Transaction that) {
+        return Double.compare(this.amount, that.amount);
+    }
+
     public boolean equals(Object x) {
-        if (this == x)  return true;
-        if (x == null)  return false;
-        if (this.getClass() != x.getClass())    return false;
+        if (this == x) return true;
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
         Transaction that = (Transaction) x;
         return (this.amount == that.amount)
                 && (this.when.equals(that.when))
-                &&(this.who.equals(that.who));
+                && (this.who.equals(that.who));
     }
 }
